a3499902bca36fd3cca17eae9fb3502e42efd0e9
ahess <ahess@scm.openanalytics.eu>
2023-04-05 11:19:34 +0200

    added main folder to improve package structure

---
+++ development/project_dev.R
@@ -0,0 +1,18 @@
+
+
+roxygen2::roxygenize("tinytest2JUnit")
+#devtools::build("project_pkg")
+#devtools::install('project_pkg')
+
+#library(projectPkg)
+#library(testthat)
+
+#test_check("project_pkg")
+#run all tests in package
+#devtools::test('project_pkg')
+
+#path <- file.path(pkg_root(), "project_pkg/tests/testthat")
+#test_file(file.path(path, "test-fetchers.R"), reporter = default_reporter())
+#test_file(file.path(path, "test-gwas.R"), reporter = default_reporter())
+
+
