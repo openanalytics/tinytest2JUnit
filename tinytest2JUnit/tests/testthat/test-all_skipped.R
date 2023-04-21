b008202655b836e9f2d2d6fc73990cf61cbe5c64
ahess <ahess@scm.openanalytics.eu>
2023-04-21 09:47:42 +0200

    removed testthat folders

--- tinytest2JUnit/tests/testthat/test-all_skipped.R
+++
@@ -1,6 +0,0 @@
-
-test_that('skip_all_tests', {
-			
-skip("it's Monday")
-
-})
--- tinytest2JUnit/tests/testthat/test-partial_skipped.R
+++
@@ -1,9 +0,0 @@
-
-test_that('skip_partial_tests', {
-			
-testthat::expect_true(TRUE)
-
-skip_if_not(TRUE)
-
-expect_true(FALSE, "this should not be run")
-})
