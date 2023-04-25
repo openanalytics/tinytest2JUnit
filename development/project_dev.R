c0f65bb9f7ba2961d4fbd583bea896b3d9bf66b2
ahess <ahess@scm.openanalytics.eu>
2023-04-25 15:44:42 +0200

    try test_package with verbose=F

--- development/project_dev.R
+++ development/project_dev.R
@@ -10,8 +10,25 @@ summary(out)
 tinytest::run_test_dir(system.file("tinytest", package="tinytest"))
 
 # possible ways to run tests on tinytest2JUnit
-tinytest::test_package("tinytest2JUnit", testdir = "inst/tinytest")	
-tinytest::run_test_dir(system.file("tinytest", package="tinytest2JUnit"))
+out1 <- tinytest::test_package("tinytest2JUnit", verbose = F)	
+out2 <- tinytest::run_test_dir(system.file("tinytest", package="tinytest2JUnit"))
+
+tmpFile1 <- tempfile(fileext = ".xml")
+tmpFile2 <- tempfile(fileext = ".xml")
+writeJUnit(out, tmpFile1)
+writeJUnit(out2, tmpFile2)
+xml1 <- xmlParse(tmpFile1)
+xml2 <- xmlParse(tmpFile2)
+expect_identical(xml1, xml2)
+expect_equal(xml1, xml2)
+
+#> expect_identical(xml1, xml2)
+#----- FAILED[attr]: <-->
+#		call| expect_identical(xml1, xml2)
+#diff| TRUE 
+#> expect_equal(xml1, xml2)
+#----- PASSED      : <-->
+#		call| expect_equal(xml1, xml2)
 
 res1 <- tinytest::run_test_file(system.file("tinytest/test_partial_skipped.R", package="tinytest2JUnit"))
 res2 <- tinytest::run_test_file(system.file("tinytest/test_everything_skipped.R", package="tinytest2JUnit"))
