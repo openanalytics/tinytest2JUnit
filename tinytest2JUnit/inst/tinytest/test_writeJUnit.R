efd88eafd665c62782dc37d4d86b2b7f721d6f65
ahess <ahess@scm.openanalytics.eu>
2023-05-11 16:52:33 +0200

    added all missing pkg dependencies

--- tinytest2JUnit/inst/tinytest/test_writeJUnit.R
+++ tinytest2JUnit/inst/tinytest/test_writeJUnit.R
@@ -36,26 +36,26 @@ if(file.exists(tmpFile)) stop("File should not have existed!")
 randomTestFolder <- sample(foldersToTest, size  = 1)
 testresults <- tinytest::run_test_dir(randomTestFolder, verbose = F)
 
-writeJUnit(tinytests = testresults, file = tmpFile)
+tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile)
 nLinesWrittenFirstTime <- length(readLines(tmpFile))
 
-writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
+tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
 nLinesWrittenSecondTime <- length(readLines(tmpFile))
 
 expect_equal(
   nLinesWrittenFirstTime, nLinesWrittenSecondTime, 
   info = "Overwrite = TRUE does overwrite the file")
 
-expect_error(writeJUnit(tinytests = testresults, file = tmpFile, overwrite = FALSE),
+expect_error(tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = FALSE),
   info = "Overwrite = FALSE does indeed not overwrite a file if it already exists.")
 
 # Test writeJUnit on empty tinytest results
 
 testresults <- tinytest::test_package("tinytest2JUnit", testdir = file.path(system.file("example_tests/empty_test_folder", package =  "tinytest2JUnit")), verbose = F)
-writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
+tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
 
 expect_true(	
-		writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE),
+		tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE),
 		info = "writeJUnit() succeeded for empty directory")
 expect_true(file.exists(tmpFile))
 
