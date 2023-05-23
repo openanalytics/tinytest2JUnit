69af5ff7f75d8bc892d7f724dcf4a1d45ebc34ec
lennart tuijnder <lennart.tuijnder@openanalytics.eu>
2023-05-23 16:00:48 +0200

    fix: update ci code load exported functions of pkg to test
    In tinytest one can write tests with the assumption that they are
    functions are already loaded. R CMD Check does that for us when it runs
    the test. However, it is explicitly stated in the documentation of
    tinytest::run_test_dir that it is assumed that all the function are
    already loaded.
    
    This commit ensures that the assumption is satisfied. For package that
    would need to get tested in the pipeline.
    
    To verify that we are doing it correctly, this commit also removes the
    `tinytest2JUnit::` for it's single exported function writeJUnit.

--- tinytest2JUnit/inst/tinytest/test_writeJUnit.R
+++ tinytest2JUnit/inst/tinytest/test_writeJUnit.R
@@ -16,7 +16,7 @@ for (i in seq_along(foldersToTest)) {
   testresults <- tinytest::run_test_dir(foldersToTest[i], verbose = F)
 
   expect_true(	
-    tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile),
+    writeJUnit(tinytests = testresults, file = tmpFile),
     info = paste0("writeJUnit() succeeded for directory: ", folderName[i])
   )
   expect_true(file.exists(tmpFile))
@@ -36,26 +36,26 @@ if(file.exists(tmpFile)) stop("File should not have existed!")
 randomTestFolder <- sample(foldersToTest, size  = 1)
 testresults <- tinytest::run_test_dir(randomTestFolder, verbose = F)
 
-tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile)
+writeJUnit(tinytests = testresults, file = tmpFile)
 nLinesWrittenFirstTime <- length(readLines(tmpFile))
 
-tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
+writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
 nLinesWrittenSecondTime <- length(readLines(tmpFile))
 
 expect_equal(
   nLinesWrittenFirstTime, nLinesWrittenSecondTime, 
   info = "Overwrite = TRUE does overwrite the file")
 
-expect_error(tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = FALSE),
+expect_error(writeJUnit(tinytests = testresults, file = tmpFile, overwrite = FALSE),
   info = "Overwrite = FALSE does indeed not overwrite a file if it already exists.")
 
 # Test writeJUnit on empty tinytest results
 
 testresults <- tinytest::test_package("tinytest2JUnit", testdir = file.path(system.file("example_tests/empty_test_folder", package =  "tinytest2JUnit")), verbose = F)
-tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
+writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE)
 
 expect_true(	
-		tinytest2JUnit::writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE),
+		writeJUnit(tinytests = testresults, file = tmpFile, overwrite = TRUE),
 		info = "writeJUnit() succeeded for empty directory")
 expect_true(file.exists(tmpFile))
 
