438b6348e63d45d63575b375cade295292d18d61
ahess <ahess@scm.openanalytics.eu>
2023-06-12 11:40:12 +0200

    fix: add example test folder to have examples run during R CMD check@

---
+++ tinytest2JUnit/inst/example_tests/multiple_files/test_file1.R
@@ -0,0 +1,10 @@
+
+
+heavy_calculation <- function(){
+  
+  Sys.sleep(runif(1,0,1)) # sleep for 0 to 1 seconds
+  TRUE
+}
+
+expect_true(heavy_calculation())
+expect_true(TRUE, info = "Some info")
---
+++ tinytest2JUnit/inst/example_tests/multiple_files/test_file2.R
@@ -0,0 +1,17 @@
+
+
+heavy_calculation <- function(){
+  
+  Sys.sleep(runif(1,0,1)) # sleep for 0 to 1 seconds
+  TRUE
+}
+
+expect_false(
+  if(heavy_calculation()){
+    TRUE
+  }else{
+    FALSE
+  }
+)
+
+
